c24668f25024258a38354ff320a0dc3b943a6e58
pxr687 <pxr687@bham.ac.uk>
2023-12-01 13:31:13 +0000

    update repo

--- cost_function_utils.py
+++ cost_function_utils.py
@@ -1,12 +1,18 @@
-# this file contains functions related to the manual implementation of the 
+# this module contains functions related to the manual implementation of the 
 # logistic regression cost function
 import numpy as np
 
-def logit_transfromation():
-    return
+# this is just to demonstrate how functions from this file can be used in 
+# "showcase_notebook.Rmd" - you can delete this function 
+def junk_function():
+    """This is just a junk function to show how you can import functions from this
+    file into your `showcase_notebook`"""
+    print('The function that produced this printout was imported from `cost_function_utils.py`')
 
 def inverse_logit():
+    # your code here
     return
 
 def logistic_regression_cost_function():
+    # your code here
     return 
--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,12 @@
 # this script performs logistic regression analysis of the CKD data
+import cost_function_utils
 import pandas as pd
 import numpy as np
-import cost_function_utils
 from IPython.display import display
+
+# this just shows how to use your `cost_function_utils` module with the current
+# script
+cost_function_utils.junk_function()
 
 # generate some toy data
 data = pd.DataFrame({'A': np.random.normal(100, 10, 100),
