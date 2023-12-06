d63632e614561b7b23d51522ec2c66bcafe8822d
mysteryvishal <vishalmistry51@gmail.com>
2023-12-06 20:24:23 +0000

    cost func utils

--- cost_function_utils.py
+++ cost_function_utils.py
@@ -9,10 +9,22 @@ def junk_function():
     file into your `showcase_notebook`"""
     print('The function that produced this printout was imported from `cost_function_utils.py`')
 
-def inverse_logit():
-    # your code here
-    return
+def inverse_logit(v):
+    """ Reverse logit transformation on array `v`"""
+    return np.exp(v) / (1 + np.exp(v)) # Reverse the log operation and odds operation.
 
-def logistic_regression_cost_function():
-    # your code here
-    return 
+def logistic_regression_cost_function(int_slo, x_val, y_val):
+
+    # unpack variables
+    intercept, slope = int_slo
+
+    # predicted vals for log-odds straight line
+    predicted_logg_odds = intercept + slope * x_val
+
+    # converting straight line predictions to a sigmoid probability curve
+    predicted_probs = inverse_logit(predicted_logg_odds)
+
+    # calc prediction error
+    sigmoid_err = y_val - predicted_probs
+
+    return np.sum(sigmoid_err**2)
