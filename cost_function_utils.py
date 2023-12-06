bd0f63b6b18c7651d6eaca912b76ae277ee8b177
luciaenelesikar <lucia.ene-lesikar@lis.ac.uk>
2023-12-06 23:21:58 +0000

    updated

--- cost_function_utils.py
+++ cost_function_utils.py
@@ -9,10 +9,20 @@ def junk_function():
     file into your `showcase_notebook`"""
     print('The function that produced this printout was imported from `cost_function_utils.py`')
 
-def inverse_logit():
+def inverse_logit(v):
     # your code here
-    return
+    return np.exp(v) / (1 + np.exp(v))
 
-def logistic_regression_cost_function():
-    # your code here
-    return 
+def logistic_regression_cost_function(intercept_and_slope, x_vals, y_vals):
+    #unpacking into values 
+    intercept, slope = intercept_and_slope
+    #log odds into a straight line
+    predicted_log_odds = intercept + slope * x_vals
+    #Converting Predictions to probabilities
+    predicted_prob_of_1 = inverse_logit(predicted_log_odds)
+    # Calculating predicted probabilities 
+    predicted_prob_of_actual_scores = y_vals * predicted_prob_of_1 + (1 - y_vals) * (1 - predicted_prob_of_1)
+    # Multiplying predicted probably by actual probability
+    log_likelihood = np.sum(np.log(predicted_prob_of_actual_scores))
+    #Mean of the root of squared errors
+    return -log_likelihood
