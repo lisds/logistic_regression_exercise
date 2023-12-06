dfb653e00282cdf0d77ee893e6abd1a20a20936a
luciaenelesikar <lucia.ene-lesikar@lis.ac.uk>
2023-12-06 23:29:08 +0000

    My updatesssss! :)

--- data_analysis.py
+++ data_analysis.py
@@ -3,7 +3,11 @@
 import cost_function_utils
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 from IPython.display import display
+import plotly.express as px
+from scipy.optimize import minimize 
+import cost_function_utils
 
 # this just shows how to use your `cost_function_utils` module with the current
 # script
@@ -18,3 +22,30 @@ display(data)
 
 # create a plot
 data.plot(kind='scatter', x='A', y='B')
+
+ckd_data = pd.read_csv('CKD full.csv')
+
+ckd_data['Class_Dummy']= ckd_data['Class'].replace(['notckd', 'ckd'], [0, 1])
+
+#plot the data :)
+px.scatter(ckd_data['Class'], ckd_data ['Hemoglobin'])
+
+#Minimizaaaation!!!
+Minimization_Estimation = minimize(
+    cost_function_utils.logistic_regression_cost_function, 
+    x0=[-5, 1], 
+    args=(ckd_data['Class_Dummy'], ckd_data['Hemoglobin'])
+    )
+
+#Stats model fitting 
+log_reg_mod = smf.logit('Class_Dummy ~ Hemoglobin', data=ckd_data)
+fitted_log_reg_mod = log_reg_mod.fit()
+
+intercept_log_red_mod, slope_log_red_mod = fitted_log_reg_mod.params
+
+# these assertions are not working
+assert np.isclose(intercept_log_red_mod, Minimization_Estimation.x[0])
+assert np.isclose(slope_log_red_mod, Minimization_Estimation.x[0])
+
+print(Minimization_Estimation.x[0], Minimization_Estimation.x[1])
+print(intercept_log_red_mod, slope_log_red_mod)
